fd79d7e15e94ede655500b5ca6349cdf212d9951
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-25 19:52:25 +0600

    added post files of css and jsx and show data in ui

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react"
+import Post from "./Post";
 
 export default function Posts() {
     const [posts, setPosts] = useState([]);
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setPosts(data))
     }, [])
     return (
-        <div className="">
-            <h3>Posts: </h3>
+        <div>
+            <h3>Posts: {posts.length}</h3>
+            {
+                posts.map(post => <Post post={post}></Post>)
+            }
         </div>
     )
 }
